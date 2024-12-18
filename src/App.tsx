8d29479f00cae2fbb71a2038f045e19b6f2c5af6
shrutidhungana <shrutidhungana123@gmail.com>
2024-12-18 15:28:44 +0545

    Deployed

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import { TicTacToe } from './tic-tac-toe'
 import "./App.css";
 import logo from "./logo.svg";
 
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <div className="App">
--- src/tic-tac-toe.tsx
+++ src/tic-tac-toe.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Board } from "./board";
 import "./tic-tac-toe.sass";
 
